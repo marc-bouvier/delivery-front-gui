1faae64729a91d3079dc6d20c7ab360aa3bcf34f
marc-bouvier <m.bouvier.dev@gmail.com>
2018-02-18 17:16:56 +0100

    flex-layout

--- pages/_lang/authentication/index.vue
+++ pages/_lang/authentication/index.vue
@@ -1,3 +1,13 @@
 <template>
-<div>Authentication</div>
+<div class="d-flex h-100vh flex-column justify-content-center align-items-center">
+
+    <h1 class="flex-grow-1">Delivery Dav</h1>
+    <div class="flex-grow-1">
+        <label>#CODE</label>
+        <input />
+    </div>
+    <div class="flex-grow-2">
+        <b-btn v-t="'authentication.order'"></b-btn>
+    </div>
+</div>
 </template>
