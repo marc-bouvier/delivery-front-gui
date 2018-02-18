66290d513941f7a785648b046a91fd57a190bff3
marc-bouvier <m.bouvier.dev@gmail.com>
2018-02-18 19:12:03 +0100

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="dav-logo h-100vh">
     <nuxt/>
+    <footer><a href="https://davidson.fr" target="_blank">Davidson Consulting</a> 2018 - TODO fix scroll</footer>
   </div>
 </template>
--- pages/_lang/authentication/index.vue
+++ pages/_lang/authentication/index.vue
@@ -1,13 +1,8 @@
 <template>
-<div class="d-flex h-100vh flex-column justify-content-center align-items-center">
-
-    <h1 class="flex-grow-1">Delivery Dav</h1>
-    <div class="flex-grow-1">
-        <label>#CODE</label>
-        <input />
-    </div>
-    <div class="flex-grow-2">
-        <b-btn v-t="'authentication.order'"></b-btn>
-    </div>
+<div class="d-flex h-100vh flex-column justify-content-center align-items-center pad-inside">
+    <h1>Delivery Dav</h1>
+    <div><label >#CODE</label></div>
+    <div > <input  /></div> 
+    <div><b-btn v-t="'authentication.order'"></b-btn></div>
 </div>
 </template>
